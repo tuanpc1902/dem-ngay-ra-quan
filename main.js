aebb3bdd50d5f763589ac942e6248e5abfb4f161
tuanpc1902 <tuanpc.1902@gmail.com>
2025-11-30 07:31:22 +0700

    refactor: remove iframe and add real-time clock display

--- main.js
+++ main.js
@@ -1,26 +1,35 @@
-const zone_name = 'Asia/Ho_Chi_Minh';
 moment.locale('vi');
+const zone_name = 'Asia/Ho_Chi_Minh';
+// const zone_offset = '+07:00';
+// const zone_abbr = 'ICT';
+// const now = moment().tz(zone_name);
+
+console.log('Current time in ' + zone_name + ' is: ' + moment().format('LLLL'));
 
 let ngayRaQuanDatePicker = document.getElementById('ngayRaQuanDatePicker');
 let ngayRaQuan = document.getElementById('ngayRaQuan');
-let storageValue = localStorage.getItem('ngayRaQuanDatePicker');
-console.log ('storageValue', storageValue)
+let localStorageValue = localStorage.getItem('ngayRaQuanDatePicker');
 
-let ngayRaQuanDateVal = moment(storageValue ?? '2026-01-28').format('YYYY-MM-DD') // default to today
+console.log('localStorageValue', localStorageValue);
 
-ngayRaQuanDatePicker.value  =  moment(storageValue ?? '2026-01-28').format('YYYY-MM-DD')
+let ngayRaQuanDateVal = moment(localStorageValue ?? '2026-01-28').format(
+  'YYYY-MM-DD'
+); // default to today
 
-ngayRaQuanDatePicker.addEventListener('change',(e)=>{
-  ngayRaQuanDateVal = e.target.value
+ngayRaQuanDatePicker.value = moment(localStorageValue ?? '2026-01-28').format(
+  'YYYY-MM-DD'
+);
+
+ngayRaQuanDatePicker.addEventListener('change', (e) => {
+  ngayRaQuanDateVal = e.target.value;
   ngayRaQuan.innerText = ngayRaQuanDateVal;
-  localStorage.setItem('ngayRaQuanDatePicker', ngayRaQuanDateVal)
+  localStorage.setItem('ngayRaQuanDatePicker', ngayRaQuanDateVal);
   console.log(ngayRaQuanDateVal);
-  reloadCounts()
-})
+  reloadCounts();
+});
 
 function reloadCounts() {
-  ngayRaQuan.innerText =
-    moment(ngayRaQuanDateVal).format('LL');
+  ngayRaQuan.innerText = moment(ngayRaQuanDateVal).format('LL');
 
   document.querySelector('.timeZone').innerText = `Múi giờ: ${zone_name}`;
 
@@ -32,8 +41,21 @@ function reloadCounts() {
 
   document.getElementById('months-count').innerText =
     moment(ngayRaQuanDateVal).diff(moment(), 'months') + ' tháng';
+
+  // document.querySelector('.today').innerText = `Hôm nay là: ${moment().format(
+  //   'LLLL'
+  // )} - Múi giờ: ${zone_name}`;
 }
-reloadCounts()
+
+function startRealtimeClock() {
+  const el = document.querySelector('.today');
+
+  setInterval(() => {
+    el.innerHTML = `Hôm nay là: ` + `<span class="clock">${moment().format('LLLL:ss')}</span> - Múi giờ: ${zone_name}`;
+  }, 1000); // cập nhật mỗi 1 giây
+}
+
+startRealtimeClock();
 
 
-
+reloadCounts();
