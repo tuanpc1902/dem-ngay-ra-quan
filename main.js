889e25c54336b63b97b83a27420bdd361c9947b7
tuanpc1902 <tuanpc.1902@gmail.com>
2025-11-30 07:38:53 +0700

    refactor: clean up unused variables and improve date handling

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
 moment.locale('vi');
 const zone_name = 'Asia/Ho_Chi_Minh';
-// const zone_offset = '+07:00';
-// const zone_abbr = 'ICT';
-// const now = moment().tz(zone_name);
-
-console.log('Current time in ' + zone_name + ' is: ' + moment().format('LLLL'));
+const defaultDate = '2026-01-28';
 
 let ngayRaQuanDatePicker = document.getElementById('ngayRaQuanDatePicker');
 let ngayRaQuan = document.getElementById('ngayRaQuan');
@@ -12,13 +8,11 @@ let localStorageValue = localStorage.getItem('ngayRaQuanDatePicker');
 
 console.log('localStorageValue', localStorageValue);
 
-let ngayRaQuanDateVal = moment(localStorageValue ?? '2026-01-28').format(
+let ngayRaQuanDateVal = moment(localStorageValue ?? defaultDate).format(
   'YYYY-MM-DD'
 ); // default to today
 
-ngayRaQuanDatePicker.value = moment(localStorageValue ?? '2026-01-28').format(
-  'YYYY-MM-DD'
-);
+ngayRaQuanDatePicker.value = ngayRaQuanDateVal;
 
 ngayRaQuanDatePicker.addEventListener('change', (e) => {
   ngayRaQuanDateVal = e.target.value;
@@ -41,10 +35,6 @@ function reloadCounts() {
 
   document.getElementById('months-count').innerText =
     moment(ngayRaQuanDateVal).diff(moment(), 'months') + ' tháng';
-
-  // document.querySelector('.today').innerText = `Hôm nay là: ${moment().format(
-  //   'LLLL'
-  // )} - Múi giờ: ${zone_name}`;
 }
 
 function startRealtimeClock() {
@@ -52,6 +42,7 @@ function startRealtimeClock() {
 
   setInterval(() => {
     el.innerHTML = `Hôm nay là: ` + `<span class="clock">${moment().format('LLLL:ss')}</span> - Múi giờ: ${zone_name}`;
+    console.log('Current time in ' + zone_name + ' is: ' + moment().format('LLLL:ss'));
   }, 1000); // cập nhật mỗi 1 giây
 }
 
