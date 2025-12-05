393d1f400b3f9d9b66283a41a84d74ed23e8b065
tuanpc1902 <tuanpc.1902@gmail.com>
2025-12-05 15:44:22 +0700

    fix: format font-face declarations and update video element in index.html

--- build.js
+++ build.js
@@ -1,48 +1,49 @@
 const esbuild = require('esbuild');
-const fs = require("fs");
+const fs = require('fs');
 // const path = require("path");
 
 // // Tạo thư mục dist nếu chưa có
-if (!fs.existsSync("dist")) {
-  fs.mkdirSync("dist");
+if (!fs.existsSync('dist')) {
+  fs.mkdirSync('dist');
 }
-if (!fs.existsSync("dist/assets")) {
-  fs.mkdirSync("dist/assets");
+if (!fs.existsSync('dist/assets')) {
+  fs.mkdirSync('dist/assets');
 }
-if (!fs.existsSync("dist/lib")) {
-  fs.mkdirSync("dist/lib");
+if (!fs.existsSync('dist/lib')) {
+  fs.mkdirSync('dist/lib');
 }
 
 // 👉 1) Copy index.html từ project → dist
-let html = fs.readFileSync("index.html", "utf8");
+let html = fs.readFileSync('index.html', 'utf8');
 
-let video = fs.readFileSync("./assets/video_cstt.MOV");
+let video = fs.readFileSync('./assets/video_cstt.MOV');
 
-let moment_locale = fs.readFileSync("./lib/moment-with-locales.min.js", "utf8");
+let moment_locale = fs.readFileSync('./lib/moment-with-locales.min.js', 'utf8');
 
-let moment = fs.readFileSync("./lib/moment.min.js", "utf8");
-let favicon = fs.readFileSync("./assets/favicon.ico");
+let moment = fs.readFileSync('./lib/moment.min.js', 'utf8');
+let favicon = fs.readFileSync('./assets/favicon.ico');
 
 // Nếu bạn build ra file với tên cố định
 // html = html.replace("./assets/style.css", "./assets/style.css");
 // html = html.replace("./assets/main.css", "./assets/main.css");
 // html = html.replace("./scripts/main.js", "./main.js");
 
-fs.writeFileSync("dist/index.html", html);
-fs.writeFileSync("dist/assets/video_cstt.MOV", video);
-fs.writeFileSync("dist/assets/favicon.ico", favicon);
-fs.writeFileSync("dist/lib/moment-with-locales.min.js", moment_locale);
-fs.writeFileSync("dist/lib/moment.min.js", moment);
+fs.writeFileSync('dist/index.html', html);
+fs.writeFileSync('dist/assets/video_cstt.MOV', video);
+fs.writeFileSync('dist/assets/favicon.ico', favicon);
+fs.writeFileSync('dist/lib/moment-with-locales.min.js', moment_locale);
+fs.writeFileSync('dist/lib/moment.min.js', moment);
 
-
-esbuild.build({
-  entryPoints: ['./scripts/main.js'],
-  bundle: true,
-  minify: true,
-  outfile: 'dist/scripts/main.js',
-  loader: { '.js': 'js' },
-  sourcemap: false,
-}).catch(() => process.exit(1));
+esbuild
+  .build({
+    entryPoints: ['./scripts/main.js'],
+    bundle: true,
+    minify: true,
+    outfile: 'dist/scripts/main.js',
+    loader: { '.js': 'js' },
+    sourcemap: false,
+  })
+  .catch(() => process.exit(1));
 
 esbuild.build({
   entryPoints: ['./assets/style.css'],
